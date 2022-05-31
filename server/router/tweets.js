7bad07a2127cb9ca01ddc95877dfd6f39d5c59a1
anniemon <seojung19@gmail.com>
2022-05-31 21:58:41 +0900

    add: express validator to tweet post/put api

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,7 +1,17 @@
 import express from "express";
 import "express-async-errors";
+import { body } from "express-validator";
 import * as controller from "../controller/index.js";
+import { validate } from "../middleware/validator.js";
 const router = express.Router();
+
+const tweetValidator = [
+  body("text")
+    .trim()
+    .isLength({ min: 3, max: 140 })
+    .withMessage("text should be between 3 and 140 characters"),
+  validate,
+];
 
 // GET /tweets
 // GET /tweets?username=:username
@@ -10,11 +20,11 @@ router.get("/", controller.getTweets);
 // GET /tweets/:id
 router.get("/:id", controller.getTweetsById);
 
-// POST /tweeets
-router.post("/", controller.postTweet);
+// POST /tweets
+router.post("/", tweetValidator, controller.postTweet);
 
 // PUT /tweets/:id
-router.put("/:id", controller.updateTweet);
+router.put("/:id", tweetValidator, controller.updateTweet);
 
 // DELETE /tweets/:id
 router.delete("/:id", controller.deleteTweet);
