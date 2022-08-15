e19536fe72d683281691b56e1e1059bcbe0f9117
anniemon <seojung19@gmail.com>
2022-08-16 00:48:33 +0900

    feat: authorize by permissions

--- server/controller/tweet.js
+++ server/controller/tweet.js
@@ -28,16 +28,26 @@ export async function postTweet(req, res) {
 export async function updateTweet(req, res) {
   const id = req.params.id;
   const text = req.body.text;
-  const tweet = await TweetRepository.update(id, text);
-  if (tweet) {
-    res.status(200).json(tweet);
-  } else {
-    res.status(404).json({ message: `Tweet id(${id}) not found` });
+  const tweet = await TweetRepository.findById(id);
+  if (!tweet) {
+    return res.sendStatus(404);
   }
+  if (tweet.userId !== req.userId) {
+    return res.status(403).json({ message: `no permissions` });
+  }
+  const updated = await TweetRepository.update(id, text);
+  return res.status(200).json(updated);
 }
 
 export async function deleteTweet(req, res) {
   const id = req.params.id;
+  const tweet = await TweetRepository.findById(id);
+  if (!tweet) {
+    return res.sendStatus(404);
+  }
+  if (tweet.userId !== req.userId) {
+    res.status(403).json({ message: `no permissions` });
+  }
   await TweetRepository.remove(id);
   res.sendStatus(204);
 }
