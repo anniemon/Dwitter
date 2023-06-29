3144ab6d029875008a4a040d4722075be44ab3ef
anniemon <seojung19@gmail.com>
2023-06-29 15:42:35 +0900

    database: use raw sql

--- server/data/users.js
+++ server/data/users.js
@@ -1,63 +1,15 @@
-let users = [
-  {
-    id: "1",
-    createdAt: new Date().toString(),
-    name: "Bob",
-    username: "bob",
-    email: "kelly@gmail.com",
-    password: "",
-    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
-  },
-  {
-    id: "2",
-    createdAt: new Date().toString(),
-    name: "Ellie",
-    username: "ellie",
-    email: "ellie@example.com",
-    password: "$2b$10$h1mNHkpQGgilGQu0UYeoC.K2PSoNStFHFuPg7ZzrZ9rW2fZ9x78RC", //1234asdf
-    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
-  },
-  {
-    id: "3",
-    createdAt: new Date().toString(),
-    name: "hey",
-    username: "hello",
-    email: "hello@gmail.com",
-    password: "$2b$10$h1mNHkpQGgilGQu0UYeoC.K2PSoNStFHFuPg7ZzrZ9rW2fZ9x78RC",
-  },
-];
+import { db } from '../db/database.js'
 
 export async function create(name, username, password, email, url = "") {
-  const newUser = {
-    id: new Date().toString(),
-    createdAt: new Date(),
-    name,
-    username,
-    password,
-    email,
-    url,
-  };
-  users = [...users, JSON.parse(JSON.stringify(newUser))];
-  return newUser.id;
+  return db.execute(`INSERT INTO users (name, username, password, email, url) VALUES (?,?,?,?,?)`,
+  [name, username, password, email, url]).then(result => result[0].insertId)
 }
 
 export async function get(username, requirePassword = false) {
-  const user = users.find((user) => user.username === username);
-  if (!user) return null;
-  else {
-    if (requirePassword) {
-      return user;
-    }
-    const payload = JSON.parse(JSON.stringify(user));
-    delete payload.password;
-    return payload;
-  }
+  return db.execute(`SELECT * FROM users WHERE username=?`, [username])
+  .then(result => result[0][0]);
 }
 
 export async function findById(id) {
-  const user = users.find((user) => user.id === id);
-  if (!user) return null;
-  const payload = JSON.parse(JSON.stringify(user));
-  delete payload.password;
-  return payload;
+  return db.execute(`SELECT * FROM users WHERE id=?`, [id]).then(result => result[0][0]);
 }
